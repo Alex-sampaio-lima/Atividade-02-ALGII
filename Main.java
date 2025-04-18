6cf7838fb85a65f43e3d8785cfc8985af4005ff2
Alexsander Sampaio <alexsandersampaiolima12@gmail.com>
2025-04-17 22:04:09 -0300

    add Dicionario Samuel resolve code

--- Main.java
+++ Main.java
@@ -1,42 +1,52 @@
-/*
-Entrega a Atividade 2 - Algoritmos e Programação II
-
-Nós,
-
-Alexsander Sampaio Lima 
-Ana Julia Ferreira Lima 
-Sthephany Viana da Silva 
-
-declaramos que
-
-todas as respostas são fruto de nosso próprio trabalho,
-não copiamos respostas de colegas externos à equipe,
-não disponibilizamos nossas respostas para colegas externos ao grupo e
-não realizamos quaisquer outras atividades desonestas para nos beneficiar ou prejudicar outros.
-*/
-
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-        Scanner read = new Scanner(System.in);
 
-        String[] palavras = new String[1000];
-
+        String[] dicionario = new String[1000];
         File arquivo = new File("teste.txt");
-
-        System.out.println(arquivo);
-
         Scanner leitorArquivo = new Scanner(arquivo);
+        String palavra = "";
+        int count = 0;
+        int i = 0;
+        String linha = "";
+        boolean palavraExistente = false;
 
-        String linha;
         while (leitorArquivo.hasNext()) {
-            linha = leitorArquivo.nextLine();
-            System.out.println(linha);
-        }
-        leitorArquivo.close();
+            linha = leitorArquivo.next().toLowerCase();
 
+            count++;
+
+            palavraExistente = false;
+            for (int j = 0; j < i; j++) {
+                if (dicionario[j].equals(linha)) {
+                    palavraExistente = true;
+                    break;
+                }
+            }
+
+            if (!palavraExistente) {
+                dicionario[i] = linha;
+                i++;
+
+                for (int j = 0; j < i - 1; j++) {
+                    for (int k = 0; k < i - 1 - j; k++) {
+                        if (dicionario[k].compareTo(dicionario[k + 1]) > 0) {
+                            String temp = dicionario[k];
+                            dicionario[k] = dicionario[k + 1];
+                            dicionario[k + 1] = temp;
+                        }
+                    }
+                }
+            }
+        }
+
+        for (int j = 0; j < i; j++) {
+            System.out.println(dicionario[j]);
+        }
+        System.out.println("Total de palavras diferentes no dicionarios = " + i);
+        leitorArquivo.close();
     }
 }
